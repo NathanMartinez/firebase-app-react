1919e1cbc9b4cc20d0ac12c480ab756b2a761a7f
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-25 19:34:04 -0700

    Refactered code. Added react bootstrap

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,27 +2,33 @@ import { useRef } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button, Card } from "react-bootstrap";
 
-export default function ForgotPassword() {
+export default function Login() {
   const emailRef = useRef();
+  const passwordRef = useRef();
 
   return (
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
+          <h2 className="text-center mb-4">Log In</h2>
           <Form>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
+            <Form.Group id="email">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
             <Button className="w-100" type="Submit">
-              Send password link
+              Log In
             </Button>
           </Form>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        <Link to="/login">Already have an account? Log In</Link>
+      <div className="d-flex flex-column w-100 text-center mt-2">
+        <Link to="/signup">Don't have an account? Sign Up</Link>
+        <Link to="/forgot-password">Forgot Password</Link>
       </div>
     </>
   );
