399b99204fd4fa1dd348827ef99779810bce3915
Nate Martinez <natemartinez@Nates-iMac.local>
2020-11-25 23:42:15 -0700

    Completed Tutorial from https://www.youtube.com/watch?v=PKwu15ldZ7k&t=2827s

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth"
 
 
-const app = initializeApp({
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
